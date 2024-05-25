85586edf2e82ecd511ebc64cc9229b17c8eb32b8
Hawk Tian <itonvin@gmail.com>
2024-05-25 10:58:34 +0800

    update keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,15 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
---
 
 local pam = vim.keymap.del
 pam("n", "<C-L>")
-pam("n", "<leader>ww")
+pam("n", "<S-L>")
+pam("n", "<S-H>")
 
 local map = vim.keymap.set
-map("n", "<leader>ww", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+map("n", "<leader>w", "<cmd>w<cr><esc>", { desc = "Save File" })
+map("n", "<leader>q", "<cmd>qa<cr><esc>", { desc = "quit" })
 map("n", "gl", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 map("n", "gh", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "<C-L>", "<cmd>!/usr/bin/php -l %<cr>", { desc = "PHP Synctax Check" })
