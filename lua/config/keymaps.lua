5e896a863d126f88740c1dc7cdbf8ae30d845ddb
Hawk Tian <itonvin@gmail.com>
2024-06-02 22:16:58 +0800

    map ctrl f to php -f

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,7 +10,9 @@ pam("n", "<S-H>")
 local map = vim.keymap.set
 
 map("n", "<leader>w", "<cmd>w<cr><esc>", { desc = "Save File" })
+map("n", "<leader>ww", "<cmd>w<cr><esc>", { desc = "Save File" })
 map("n", "<leader>q", "<cmd>qa<cr><esc>", { desc = "quit" })
 map("n", "gl", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 map("n", "gh", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "<C-L>", "<cmd>!/usr/bin/php -l %<cr>", { desc = "PHP Synctax Check" })
+map("n", "<C-F>", "<cmd>!/usr/bin/php -f %<cr>", { desc = "Execute PHP Code" })
